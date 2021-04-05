1658b8fde8501c735036fd9bf5a646916bbd3f6c
Ivan Zapata <falenyo@gmail.com>
2021-04-05 16:22:45 -0500

    Included connection to MySQL

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+# from forms import AddPuppy, DelPuppy, AddOwner, DelOwner
+from flask import Flask, redirect, render_template, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+### MySQL DATABASE SECTION ###
+
+from config import username, password
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{username}:{password}@server/db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+## Defining db
+db = SQLAlchemy(app)
+Migrate(app,db)
