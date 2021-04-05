2da061ab4b48afcc891a1a8a68c0b1dddf0dfcf7
Ivan Zapata <falenyo@gmail.com>
2021-04-05 17:05:09 -0500

    Connecting OK

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 ### MySQL DATABASE SECTION ###
 import config
-from config import username, password
+from config import username, password, server, db
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{username}:{password}@server/db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{username}:{password}@{server}/{db}'
 
 ## Defining db
 db = SQLAlchemy(app)
 Migrate(app,db)
 
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(db.String(128))
+
+if __name__ == "__main__":
+    app.run(debug=True)
