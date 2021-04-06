e7d7d2a54678f1a4a97be5e113250f82d99b2076
Ivan Zapata <falenyo@gmail.com>
2021-04-05 22:30:36 -0500

    Error testing OK

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from flask import Flask, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import *
+from sqlalchemy.exc import NoSuchTableError
 
 app = Flask(__name__)
 
@@ -11,28 +13,30 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 
 ### MySQL DATABASE SECTION ###
 import config
-from config import username, password, server, db
+from config import username, password, server, database, db_uri
 
 # basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{username}:{password}@{server}/{db}'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 
 ## Defining db
 db = SQLAlchemy(app)
 Migrate(app,db)
 
+
 class Owner(db.Model):
 
     __tablename__ = 'owner'
 
-    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text)
     address = db.Column(db.Text)
     city = db.Column(db.Text)
     state = db.Column(db.Text)
     zipcode = db.Column(db.Numeric(5,0))
 
-    def __init__(self, name, address, city, state, zipcode):
+    def __init__(self, owner_id, name, address, city, state, zipcode):
+        self.owner_id = owner_id
         self.name = name
         self.address = address
         self.city = city
@@ -44,15 +48,16 @@ class Puppy(db.Model):
     
     __tablename__ = 'puppies'
     
-    id = db.Column(db.Integer, primary_key = True)
-    owner_id = db.Column(db.Integer, db.ForeignKey(Owner.id))
+    puppy_id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey(Owner.owner_id), nullable = False)
     name = db.Column(db.Text)
     nickname = db.Column(db.Text)
     height_in_inches = db.Column(db.Numeric(2,1))
     color = db.Column(db.Text)
     favorite_food = db.Column(db.Text)
 
-    def __init__(self, name, nickname, height_in_inches, color, owner, favorite_food):
+    def __init__(self, puppy_id, name, nickname, height_in_inches, color, owner, favorite_food):
+        self.puppy_id = puppy_id
         self.name = name
         self.nickname = nickname
         self.height_in_inches = height_in_inches
@@ -61,8 +66,24 @@ class Puppy(db.Model):
         self.favorite_food = favorite_food
 
 
-db.create_all()
+# Query to select all data from table puppies
+sql = 'SELECT * FROM puppies'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Creating MySQL engine
+engine = create_engine(db_uri)
+
+# Opening a session to throw try/except error
+session = db.session()
+
+# Validating if sql string will throw a try/except error
+# If it throws an error, .create_all() method will execute
+try: 
+    cursor = session.execute(sql).cursor
+    if(cursor):
+        print('connected')
+    else: 
+        print('table does not exist')
+except:
+    db.create_all()
+
 
