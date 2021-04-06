4fb12ed85d1037f393f9cb0048db4b0006ae4cac
Ivan Zapata <falenyo@gmail.com>
2021-04-06 11:20:53 -0500

    Forms posting to MySQL OK

--- forms.py
+++ forms.py
@@ -3,14 +3,6 @@ from wtforms import *
 
 class AddPup(FlaskForm):
 
-    # owner_id = form.owner_id.data
-    #     puppy_name = form.puppy_name.data
-    #     age = form.age.data
-    #     gender = form.gender.data
-    #     height_inches = form.height_inches.data
-    #     color = form.color.data
-    #     favorite_food = form.favorite_food.data
-
     owner_id = IntegerField("Owner's ID: ")
     puppy_name = StringField("Name: ")
     age = IntegerField("Age: ")
@@ -20,4 +12,13 @@ class AddPup(FlaskForm):
     favorite_food = StringField("Favorite Food: ")
     submit = SubmitField('Add')
 
+
+class AddOwner(FlaskForm):
+
+    owner_name = StringField("Name: ")
+    address = StringField("Address: ")
+    city = StringField("City: ")
+    state = StringField("State: ")
+    zipcode = IntegerField("Zip Code: ")
+    submit = SubmitField('Add')
 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import *
 from sqlalchemy.exc import NoSuchTableError
-from forms import AddPup
+from forms import AddPup, AddOwner
 
 
 app = Flask(__name__)
@@ -26,35 +26,27 @@ db = SQLAlchemy(app)
 Migrate(app,db)
 
 
-# class Owner(db.Model):
+class Owner(db.Model):
 
-#     __tablename__ = 'owner'
+    __tablename__ = 'owner'
 
-#     owner_id = db.Column(db.Integer, primary_key = True)
-#     owner_name = db.Column(db.Text)
+    owner_id = db.Column(db.Integer, primary_key = True)
+    owner_name = db.Column(db.Text)
+    address = db.Column(db.Text)
+    city = db.Column(db.Text)
+    state = db.Column(db.Text)
+    zipcode = db.Column(db.Numeric(5))
 
-
-#     def __init__(self, owner_name):
-#         self.owner_name = owner_name
+    def __init__(self, owner_name, address, city, state, zipcode):
+        self.owner_name = owner_name.lower()
+        self.address = address.lower()
+        self.city = city.lower()
+        self.state = state.lower()
+        self.zipcode = zipcode
     
 
-#     def __repr__(self):
-#         return f"The owner's name is {self.owner_name}."
-
-    # owner_id = db.Column(db.Integer, primary_key = True)
-    # name = db.Column(db.Text)
-    # address = db.Column(db.Text)
-    # city = db.Column(db.Text)
-    # state = db.Column(db.Text)
-    # zipcode = db.Column(db.Numeric(5,0))
-
-    # def __init__(self, owner_id, name, address, city, state, zipcode):
-    #     self.owner_id = owner_id
-    #     self.name = name
-    #     self.address = address
-    #     self.city = city
-    #     self.state = state
-    #     self.zipcode = zipcode
+    def __repr__(self):
+        return f"The owner's name is {self.owner_name}."
 
 
 class Puppy(db.Model):
@@ -62,7 +54,7 @@ class Puppy(db.Model):
     __tablename__ = 'puppies'
     
     puppy_id = db.Column(db.Integer, primary_key = True)
-    owner_id = db.Column(db.Integer)
+    owner_id = db.Column(db.Integer, ForeignKey(Owner.owner_id), nullable = False)
     puppy_name = db.Column(db.Text)
     age = db.Column(db.Numeric(2,0))
     gender = db.Column(db.Text)
@@ -82,28 +74,13 @@ class Puppy(db.Model):
     def __repr__(self):
         return f"{self.puppy_name}'s favorite food is {self.favorite_food}."
 
-    # nickname = db.Column(db.Text)
-    # height_in_inches = db.Column(db.Numeric(2,1))
-    # color = db.Column(db.Text)
-    # favorite_food = db.Column(db.Text)
-    # owner = db.Column(db.Text)
-
-    # def __init__(self, puppy_id, owner_id, name, nickname, height_in_inches, color, favorite_food, owner):
-    #     self.puppy_id = puppy_id
-    #     self.owner_id = owner_id
-    #     self.name = name
-    #     self.nickname = nickname
-    #     self.height_in_inches = height_in_inches
-    #     self.color = color
-    #     self.favorite_food = favorite_food
-    #     self.owner = owner
-
+   
     # def __repr__(self):
     #     return f'Puppy\'s name is {self.name}. \n And has been adopted by {self.owner}. \n S/he loves {self.favorite_food} and likes to be called by {self.nickname}! \n \n Here are some facts! \n ID: {self.puppy_id} \n Furr Color: {self.color} \n Height (in): {self.height_in_inches}'
 
 
 # Query to select all data from table puppies and force an error to create tables if needed
-sql = 'SELECT * FROM puppies'
+sql = 'SELECT * FROM puppies INNER JOIN owner'
 engine = create_engine(db_uri)
 session = db.session()
 
@@ -125,6 +102,26 @@ except:
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/add_owner', methods=['GET', 'POST'])
+def add_owner():
+
+    form = AddOwner()
+
+    if form.validate_on_submit():
+
+        owner_name = form.owner_name.data
+        address = form.address.data
+        city = form.city.data
+        state = form.state.data
+        zipcode = form.zipcode.data
+        added_owner = Owner(owner_name, address, city, state, zipcode)
+        db.session.add(added_owner)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    
+    return render_template('add_owner.html', form = form)
 
 
 @app.route('/add_pup', methods=['GET','POST'])
@@ -148,7 +145,7 @@ def add_pup():
 
         return redirect(url_for('index'))
 
-    return render_template('add.html', form = form)
+    return render_template('add_puppy.html', form = form)
 
 
 
