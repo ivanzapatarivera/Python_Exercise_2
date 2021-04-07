f7a33c23aff232778489ed76441853ca44c148f3
Ivan Zapata <falenyo@gmail.com>
2021-04-06 20:36:01 -0500

    Capitalized owner names OK

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ try:
     cursor = session.execute(sql).cursor
     if(cursor):
         print('CONNECTED')
-        db.create_all()
+       
 except:
     print(f'Table does not exist and string {sql} was not executed.')
     print(f'*** CREATING TABLE ***')
@@ -129,7 +129,7 @@ def add_pup():
 
     form = AddPup()
     
-    owner_query = session.query(Owner.owner_id, Owner.owner_name)
+    # owner_list = session.query(Owner.owner_id, Owner.owner_name)
     # owner_list = session.execute('SELECT owner_id, CONCAT(UCASE(LEFT(owner_name,1)), LCASE(SUBSTRING(owner_name, 2))) AS owner_name FROM python_exercise_2.owner')
     
     # our_user = session.query(User).filter_by(name='ed').first() 
@@ -150,8 +150,10 @@ def add_pup():
         db.session.commit()
 
         return redirect(url_for('index'))
- 
-    return render_template('add_puppy.html', form = form, owner_list = owner_query, gender_list = gender_list)
+    
+    owner_list = Owner.query.all()
+    print(owner_list)
+    return render_template('add_puppy.html', form = form, owner_list = owner_list, gender_list = gender_list)
 
 
 
